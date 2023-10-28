75e148ce360361044e7005f0fa0df064a6210466
Le Huong Giang <111722560+gxdumplingg@users.noreply.github.com>
2023-10-28 15:20:03 +0700

    Update testingJava.java

--- src/testingJava.java
+++ src/testingJava.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class testingJava {
+import java.util.*;
+import java.util.Scanner;
+import java.lang.Math;
+public class testingJava {
+    static double r;
+    public static void calc() {
+        double chuVi = 2 * r * Math.PI;
+        double dienTich = r * r * Math.PI;
+        System.out.println(chuVi);
+        System.out.println(dienTich);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        r = sc.nextDouble();
+        calc();
+    }
+    public static void main2(String[] args) {
+        int n = 10;
+        System.out.println(n);
+        
+    }
 }
